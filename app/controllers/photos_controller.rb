9002057286a39610cde771d7f0f7c88e28b41147
Matt surls <msurls@ChicagoBooth.edu>
2023-05-06 00:35:42 +0000

    Complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,7 +5,8 @@ class PhotosController < ApplicationController
   end
 
   def create
-    user_id = session.fetch(:user_id)
+    user_id = session.fetch("user_id")
+    # user_id = params.fetch("input_owner_id")
     image = params.fetch("input_image")
     caption = params.fetch("input_caption")
     photo = Photo.new
@@ -30,6 +31,7 @@ class PhotosController < ApplicationController
     redirect_to("/photos")
   end
 
+  
   def update
     id = params.fetch("the_photo_id")
     photo = Photo.where({ :id => id }).at(0)
